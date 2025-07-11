bd6c09ed482d5e64a2edaa5ac79fc0900c54fa0d
Andres Melendes <andresruse18@gmail.com>
2025-07-11 23:56:38 +0200

    completing first stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,19 +7,19 @@ public class Main {
   public static void main(String[] args) throws Exception {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.err.println("Logs from your program will appear here!");
-    
+
     String command = args[0];
     if("decode".equals(command)) {
       //  Uncomment this block to pass the first stage
-      //  String bencodedValue = args[1];
-      //  String decoded;
-      //  try {
-      //    decoded = decodeBencode(bencodedValue);
-      //  } catch(RuntimeException e) {
-      //    System.out.println(e.getMessage());
-      //    return;
-      //  }
-      //  System.out.println(gson.toJson(decoded));
+       String bencodedValue = args[1];
+       String decoded;
+       try {
+         decoded = decodeBencode(bencodedValue);
+       } catch(RuntimeException e) {
+         System.out.println(e.getMessage());
+         return;
+       }
+       System.out.println(gson.toJson(decoded));
 
     } else {
       System.out.println("Unknown command: " + command);
@@ -30,7 +30,7 @@ public class Main {
   static String decodeBencode(String bencodedString) {
     if (Character.isDigit(bencodedString.charAt(0))) {
       int firstColonIndex = 0;
-      for(int i = 0; i < bencodedString.length(); i++) { 
+      for(int i = 0; i < bencodedString.length(); i++) {
         if(bencodedString.charAt(i) == ':') {
           firstColonIndex = i;
           break;
@@ -42,5 +42,5 @@ public class Main {
       throw new RuntimeException("Only strings are supported at the moment");
     }
   }
-  
+
 }
